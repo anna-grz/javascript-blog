59386dcd28d81f208246c51e77ade3fedfd9e614
Anna Grządka <anna.grzadka@gmail.com>
2023-02-26 08:09:55 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    const clickedElement = this;
+    console.log('Link was clicked!');
+    console.log(event);
 
-/* remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
+this.classList.add('active');
 
-
-/* remove class 'active' from all articles */
+/* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.post');
 
 for(let activeArticle of activeArticles){
